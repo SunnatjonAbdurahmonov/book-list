b983bfc32ab391a30cf01e296823a53777e5cfcf
SunnatjonAbdurahmonov <abdurahmonov.sunnat77@gmail.com>
2021-08-31 18:25:20 +0500

    book-list

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const bookTitle = document.querySelector("#title"),
+    bookAuthor = document.querySelector("#author"),
+    bookYear = document.querySelector("#year"),
+    addBook = document.querySelector("#submit"),
+    bookList = document.querySelector("#book-list");
+
+  addBook.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (bookTitle == "" && bookAuthor == "" && bookYear == "") {
+      alert("OOPS!");
+    } else {
+      const newRow = document.createElement("tr");
+
+      // Create new title
+      const newTitle = document.createElement("th");
+      newTitle.innerHTML = bookTitle.value;
+      newRow.appendChild(newTitle);
+
+      // Create new author
+      const newAuthor = document.createElement("th");
+      newAuthor.innerHTML = bookAuthor.value;
+      newRow.appendChild(newAuthor);
+
+      // Create new year
+      const newYear = document.createElement("th");
+      newYear.innerHTML = bookYear.value;
+      newRow.appendChild(newYear);
+
+      bookList.appendChild(newRow);
+
+      bookTitle.value = "";
+      bookAuthor.value = "";
+      bookYear.value = "";
+    }
+  });
+});
